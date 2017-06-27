764595a1e390f4d2d6309f848425bae4a4d1d658
Adam Jimenez <jimenez.adam0@gmail.com>
2017-06-26 23:12:58 -0400

    https support

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+const fs = require('fs')
+const https = require('https')
+
 const src = require('./src')
 
 const config = src.config
 const app = src.rest
 
-app.listen(config.port, () => {
-    console.log(`Blog API listening on port ${config.port}`)
-})
+if (config.https.active) {
+    const options = {
+        key: fs.readFileSync(config.https.privateKeyPath),
+        cert: fs.readFileSync(config.https.certificatePath)
+    }
+    https.createServer(options, app).listen(config.port, () => {
+        console.log(`Blog API listening on port ${config.port} using HTTPS`)
+    })
+} else {
+    app.listen(config.port, () => {
+        console.log(`Blog API listening on port ${config.port} using HTTP`)
+    })
+}
